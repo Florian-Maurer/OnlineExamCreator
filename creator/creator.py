a26fd26bd1b23d0b0798ee8f94f207f9a37fd752
Florian-Maurer <fm082@hdm-stuttgart.de>
2020-06-17 22:24:44 +0200

    Created mechanism to randomly chose Task Version

--- creator/creator.py
+++ creator/creator.py
@@ -1,6 +1,22 @@
+import random
+
+
 def create_exams(number_of_exams, excel_dict):
-    exams = []
-    for index in range(1, number_of_exams):
+    exam_list = []
+
+    for i in range(0, number_of_exams):
+        exam = {'General Information': excel_dict['General Information']}
+        index = 1
+        for full_task in list(excel_dict.values())[1:]:
+            exam['Task ' + str(index)] = get_random_task_version(full_task)
+            index = index + 1
+
+        exam_list.append(exam)
+    return exam_list
 
 
-        return 0
+def get_random_task_version(task):
+    short_task = {'Titel': task.iloc[0][0], 'Points': task.loc[0]['Points'],
+                  'Text': random.choice(list(task['Versions']))}
+
+    return short_task
