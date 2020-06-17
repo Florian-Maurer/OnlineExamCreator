220d0e746ac5ba3974adf5e6ff7f951edecc5c6f
Florian-Maurer <fm082@hdm-stuttgart.de>
2020-06-17 22:41:56 +0200

    Reworked General Information Part of randomly created exam

--- creator/creator.py
+++ creator/creator.py
@@ -5,7 +5,8 @@ def create_exams(number_of_exams, excel_dict):
     exam_list = []
 
     for i in range(0, number_of_exams):
-        exam = {'General Information': excel_dict['General Information']}
+        turn_df_to_dict(excel_dict['General Information'])
+        exam = {'General Information': turn_df_to_dict(excel_dict['General Information'])}
         index = 1
         for full_task in list(excel_dict.values())[1:]:
             exam['Task ' + str(index)] = get_random_task_version(full_task)
@@ -20,3 +21,10 @@ def get_random_task_version(task):
                   'Text': random.choice(list(task['Versions']))}
 
     return short_task
+
+
+def turn_df_to_dict(df):
+    dictio = {}
+    for key in df.keys():
+        dictio[key] = df.loc[0][key]
+    return dictio
